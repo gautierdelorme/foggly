16e280b274e8d870474573cc717c9dc426c98d6f
Gautier Delorme <gautier.delorme@gmail.com>
2018-01-13 22:51:39 +0100

    remove unique index name on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214095108) do
+ActiveRecord::Schema.define(version: 20180113214820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 20171214095108) do
     t.string "name", default: "", null: false
     t.string "role", default: "member", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role"], name: "index_users_on_role"
   end
